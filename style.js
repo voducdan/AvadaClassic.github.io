6310d0ad63600a01761ddac1c2743e815e463d7c
Vo Duc Dan <voducdand99@gmail.com>
2018-12-02 02:57:50 +0700

    add hide header

--- style.js
+++ style.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(){
     var headermenu = document.querySelectorAll('.headermenu li a');
-    console.log(headermenu);
     for(x of headermenu){
         x.onmouseover = function(){
             this.classList.add('active');
@@ -10,6 +9,27 @@ document.addEventListener('DOMContentLoaded',function(){
         x.onmouseout = function(){
             this.classList.remove('active');
         };
+    }
+    var hidedheder = document.querySelector('#hidedheder');
+    var turnbtn = document.querySelector('.turnbtn');
+    var number=1;
+    turnbtn.onclick = function(){
+        number++;
+        this.classList.toggle('onbtn');
+        this.classList.remove('turn');
+        if(number%2==0){
+            this.classList.remove('turnbntmoveup');
+            this.classList.add('turnbntmovedown');
+            hidedheader.classList.add('appearhidedheader');
+            hidedheader.classList.remove('disappearhidedheader');
+        }
+        else{
+            this.classList.add('turnbntmoveup');
+            this.classList.remove('turnbntmovedown');
+            hidedheader.classList.add('disappearhidedheader');
+            hidedheader.classList.remove('appearhidedheader');
+
+        }
     }
 
 
